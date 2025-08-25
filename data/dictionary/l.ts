dcf04d4dc6df0b70ff38e3c50714ab137894393d
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-08-25 21:52:01 +0200

    Update l.ts

--- data/dictionary/l.ts
+++ data/dictionary/l.ts
@@ -1,2 +1,141 @@
-const rawDictionaryText = ``;
+const rawDictionaryText = `
+la s. (muz.) la. 
+labîz s. melodie. 
+labîzlî adj. melodic; melodios. 
+laborant s. laborant. 
+laborator s. laborator. // ●m e k t e p 
+l a b o r a t o r î laborator şcolar. 
+laboratorğ î s. laborant. 
+laf s. 1. vorbă; cuvânt. 2. vorbărie; 
+vorbe goale. // ●á r úw l a f m an 
+añ l a tm a k a lua pe cineva cu 
+binişorul. ●aw u z und an l a fn î n 
+a lm a k a lua cuiva vorba din gură; a 
+spune tocmai ceea ce voia să zică 
+alcineva. ●a y t k an l a f l a r î n a s a k 
+b o lm a k a-şi păzi gura; a-şi măsura 
+cuvintele. ●b a l a b an l a f a tm a k a 
+arunca vorbe mari. ●b í r l a f 
+k a ş î rm a k a scăpa o vorbă. ●e k í 
+l a f a y tm a k a spune cuiva două 
+vorbe. ●l a f a ñ l a tm a k a explica; a 
+se face înŃeles. ●l a f e tm e k a vorbi; 
+a discuta. ●l a f g e zm e k a umbla 
+vorba; a se zvoni. ●l a f k a r am a k a 
+căuta capăt de vorbă; a se da în vorbă 
+cu cineva; a trage de limbă. ●l a f 
+k a tm a k a se băga în vorbă; a 
+interveni în discuŃie. ●l a f s a tm a k a 
+vinde pielea ursului din pădure. 
+●l a f î n ğ u tm a k a-şi pune frâu la 
+limbă; a-şi Ńine limba în gură; a-şi Ńine 
+gura. ●l a f î n t a r tm a k a-şi retrage 
+cuvântul; a retracta; a se dezice. 
+●l a f î n t u tm a k a se Ńine de cuvânt. 
+●l a f î nda t u rm a k a se Ńine de 
+cuvânt. ●l a f î n dan k a y tm a k a nu 
+se Ńine de cuvânt; a-şi retrage 
+cuvântul; a retracta; a se dezice. 
+●l a f î nda n ş î km am a k a nu ieşi 
+din cuvântul cuiva. ●l a f k a 
+k í r í şm e k a se băga în vorbă; a 
+interveni în discuŃie. ●l a f k a 
+t u rm a k a sta de vorbă; a sta de 
+poveşti; a sta la taifas. ●l a f k a 
+t u tm a k a Ńine de vorbă. ●l a f n î 
+d e ñ í ş t í rm e k a schimba vorba. 
+●l a f t a n a ñ l am a k a asculta de cuvânt. // ●árúw laf — vorbă bună. ●gúzel laf — compliment. ●kurî laf — vorbă goală. ●laf ustasî — orator. ●sáde bírkaş laf bílemen — ştiu doar câteva cuvinte. ●tatlî laf — vorbe dulci. 
+lafezúw s. emfază. 
+lafsîz adj. fără vorbe. 
+lafsîzkalma s. scăpare; lapsus. 
+lafsîzlîk s. scăpare; lapsus. 
+lafşî s., adj. palavragiu; limbut. 
+lak s. (chim.) lac. 
+lakîrdagî, Arap- s. (orn.) flecar arab (lat., Turdoides squamiceps). 
+lakîrdagî, Irak- s. (orn.) flecar irakian (lat., Turdoides altirostris). 
+lakîrdak, sarîğa- s. (orn.) flecar berber (lat., Turdoides fulvus). 
+lakîrdak, sîradan- s. (orn.) flecar (lat., Turdoides caudaus). 
+lakon ik adj. laconic. 
+lakşa s. (gastron.) tăiŃei. // ●lakşa şorbasî — ciorbă cu tăiŃei. 
+laktoza s. lactoză. 
+lal I. adj. (d. ape) lin. II. s., adj. mut; necuvântător. 
+lallîk adj. muŃenie. 
+lal-sagîr s., adj. surdomut. 
+langusta s. (zool.) langustă (lat., Palinurus vulgaris). 
+lanolina s. lanolină. 
+lap s. 1. vorbă; cuvânt. 2. vorbărie; vorbe goale. 
+lapbolgan adj. discutat; relatat; spus. 
+lapezmesí s. emfază. 
+lapkaragan adj. iscoditor. 
+lapkaraw s. iscodire. 
+lapkaturma s. conversaŃie; discuŃie. 
+lapon s., adj. lapon. 
+lapsîz adj. fără vorbe. 
+lapsîzlîk s. scăpare; lapsus. 
+laser s. laser. 
+laso s. laŃ; ştreang; lasou. 
+laşîn s. (orn.) şoim călător (lat., Falco peregrinus). 
+law s., adj. laoŃian. 
+Law s. (topon.) Laos. 
+lawğa I. adv. în limba laoŃiană. II. s. (limba) laoŃiană. 
+law-law interj. gâl-gâl!; 
+lawlî s., adj. laoŃian. 
+lawuldagan adj. gâlgâitor; clocotitor. 
+lawuldama s. gâlgâit; clocot. 
+lawuldamak v.i. a gâlgâi; a clocoti. 
+lawuldaw s. gâlgâit; clocot. 
+lawultî s. gâlgâit; clocot. 
+lawureat adj. laureat. 
+lay s. 1. strat; fâşie; film; peliculă. 2. (la uşi, ferestre) canat. 3. trâmbă; nor; nor de fum; dâră de fum. // ●bulut layî — trâmbă de nori. ●duman layî — trâmbă de fum. ●toprak layî — strat de pământ. ●toz layî — peliculă de praf. 
+lay-lay adj. 1. în straturi. 2. stratiform; stratificat. 
+laylî adj. în straturi; stratiform; stratificat. 
+Laz-Mallesí s. (topon.) Lazul (jud. ConstanŃa). 
+lábirint s. labirint. 
+láden s. (bot.) călŃunaş; condurul-doamnei (lat., Tropaeolum majus). 
+láden-teresí s. (bot.) călŃunaş; condurul-doamnei (lat., Tropaeolum majus). 
+ládes s. pariu; rămăşag; prinsoare. //●ládes tutmak — a paria. 
+ládin, ak- s. (bot.) brad (lat., Abies alba). 
+ládin, gúmúş- s. (bot.) brad argintiu (lat., Abies concolor). 
+ládin, kîrmîzî- s. (bot.) brad roşu de California (lat., Abies magnifica). 
+ládin, kîzîl- s. (bot.) brad-roşu; brad-negru; molid; molift (lat., Picea abies). 
+ládin, soylî- s. (bot.) brad argintiu albastru; brad nobil (lat., Abies procera). 
+ládin, úyken- s. (bot.) brad de Vancouver (lat., Abies grandis). 
+ládiní, dagaşasî- s. (bot.) brad alpin (lat., Abies lasiocarpa). 
+ládiní, Japon- s. (bot.) brad japonez (lat., Abies firma). 
+ládiní, Kawkaz- s. (bot.) brad de Caucaz (lat., Abies nordmanniana). 
+ládiní, Rum- s. (bot.) brad grecesc (lat., Abies cephalonica). 
+ládiní, Sebír- s. (bot.) brad siberian (lat., Abies sibirica). 
+láğiwert-kirazkuşî s. (orn.) vrabie albastră (lat., Passerina cyanea). 
+láğiwert-súlekeş s. (orn.) vrabie albastră (lat., Passerina cyanea). 
+láğiwert-tenek s. (orn.) vrabie albastră (lat., Passerina cyanea). 
+láğíwert adj. albastru închis; bleumarin. 
+láj s. 1. medicament; doctorie. 2. (fig.) remediu; soluŃie. // ●ğatar kursakka alîngan láj — medicament care se ia înainte de culcare. ●ísteseñíz bír láj beriyím — dacă doriŃi vă dau un medicament. ●láj tedaviyí — cură. ●lájler yazî man beríle — medicamentele se dau pe bază de reŃetă. ●súrgún lájí — (med.) purgativ; laxativ. ●tîñdîruwğî láj — (med.) medicament calmant. ●yuklatkan láj — somnifer. 
+lájlí adj. 1. remediabil; curabil. 2. medicamentos. 
+lájsíz adj. iremediabil; incurabil. 
+lájyazîsî s. (med.) reŃetă; prescripŃie medicală. 
+lákap s. poreclă. // ●lákap takmak — a porecli. 
+lákapatîlgan adj. poreclit; numit. 
+lákaplî adj. poreclit; aşa-zis; aşa-numit. 
+lákayt adj. indiferent; dezinteresat. 
+lákin conj. dar; însă. 
+lákîrdî s. 1. cuvânt; vorbă. 2. conversaŃie; discuŃie. 3. flecăreală; pălăvrăgeală. 
+lál I. s. 1. rubin. 2. cerneală roşie. II. s., adj. carmin; cârmâz. 
+lále s. (bot.) lalea (lat., Tulipa gesneriana). 
+lálesí, dak- s. (bot.) dediŃei; adormiŃele (lat., Pulsatilla montana). 
+lálesí, karga- s. (bot.) zămoşiŃă; macul-ciorii (lat., Hibiscus trionum). 
+lálesí, taj- s. (bot.) bibilică; coroană; lalea-pestriŃă (lat., Fritillaria meleagris). 
+láma s. (zool.) lamă (lat., Lama). 
+láma-karabaşî s. lama. 
+lámba s. 1. lampă cu petrol; felinar; semafor. 2. (fiz.) lampă. //●ğoklama lámbasî — (tehn.) lampă de control. ●lámba íşesí — sticlă de lampă. ●lámbanîñ ğarîgî uzaktan yeşíl bolîp kóríne — din 
+depărtare lumina lămpii se vede verde. ●tîlsîm lámbasî — lampion. 
+lámela s. lamelă. 
+lándo s. landou. 
+lástík s. 1. (auto.) cauciuc; cameră. 2. elastic. 3. galoş; şoşon; cizme de cauciuc. 4. gumă; radieră. 5. (auto.) anvelopă; pneu. // ●arka lástíkler cauciucurile din spate. ●bír şerík metre lástík — un sfert de metru de elastic. ●bír şípt lástík — o pereche de galoşi. ●íş lástígí hawa kaşîra — răsuflă camera. ●lástík ğanardaklaştîrîlmalî — cauciucul trebuie vulcanizat. 
+lástíklí adj. de cauciuc. 
+láşin s. (orn.) şoim călător (lat., Falco peregrinus). 
+láşinğí s. şoimar. 
+láta s. şipcă; leaŃ; scândurică. 
+látif adj. fin; gingaş; duios; delicat; graŃios. 
+Látif, El- s. (relig., arab.) "Cel subtil"; “Cel capabil să pătrundă cele mai adânci şi delicate înŃelesuri”; “Cel care acordă binefacerea cu tâlc şi gingăşie” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah).
+`;
 export default rawDictionaryText;
