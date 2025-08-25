78362b01a6d8852cf6f31be1f567c5f018d8dc04
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-08-25 20:06:56 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -50,7 +50,8 @@ const App: React.FC = () => {
             </h1>
           </div>
           <p className="text-lg text-gray-600 dark:text-gray-400">
-            A searchable Crimean Tatar - Romanian Dictionary<br>Standardization of Crimean Tatar language by University of Bucharest</br>
+            A searchable Crimean Tatar - Romanian Dictionary
+            Standardization of Crimean Tatar language by University of Bucharest
           </p>
         </header>
 
