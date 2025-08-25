95a24ceec719eb9eacd1bc2b776ea2a082f109e8
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-08-26 00:09:04 +0200

    Update k.ts

--- data/dictionary/k.ts
+++ data/dictionary/k.ts
@@ -1,4 +1,4 @@
 const rawDictionaryText = `
-Korezmiy, El- I. s. (topon.) Khwarezm. II. adj. din Khwarezm. // ● El-Korezmiy ifadesí — (fon.) ("Silabe Khwarezm") Predictibilitate.
+Korezmiy, El- I. s. (topon.) Khwarezm. II. adj. din Khwarezm. // ● El-Korezmiy ifadesí — (fon.) ("Silabe Khwarezm") Predictibilitate (termen special folosit în fonetica limbii tătare).
 `;
 export default rawDictionaryText;
