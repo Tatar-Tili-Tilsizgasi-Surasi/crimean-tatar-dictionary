1367d544ccca9e50ea06a4bf78faa55845f9c7f0
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-08-25 23:50:49 +0200

    Update k.ts

--- data/dictionary/k.ts
+++ data/dictionary/k.ts
@@ -1,4 +1,4 @@
 const rawDictionaryText = `
-Korezmiy, El- I. s. Khwarezm II. adj. khwarezmeană // ● El-Korezmiy ifadesí — (fon.) predictibilitate
+Korezmiy, El- I. s. Khwarezm II. adj. din Khwarezm // ● El-Korezmiy ifadesí — (fon.) predictibilitate
 `;
 export default rawDictionaryText;
