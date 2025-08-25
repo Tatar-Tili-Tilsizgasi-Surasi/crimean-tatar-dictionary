57533a05527a02d39264ed2100b8eb5594646f84
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-08-25 11:38:44 +0200

    Update a.ts

--- data/dictionary/a.ts
+++ data/dictionary/a.ts
@@ -107,7 +107,140 @@ Abdulla(h) s. (antrop. m., arab.) "Supusul lui Allah"; "Proslăvitorul lui Allah
 Abdullátif s. (antrop. m., arab.) “Supusul Celui subtil”.
 Abdulmağit/Abdulmajit s. (antrop. m., arab.) “Supusul Celui nobil”.
 Abdulmalik s. (antrop. m., arab.) “Supusul Atotstăpânitorului”.
+Abdulmaniy s. (antrop. m., arab.) “Supusul Celui ce preîntâmpină”. 
+Abdulmeğit/Abdulmejit s. (antrop. m., arab.) “Supusul Celui glorios”. 
+Abdulmetin s. (antrop. m., arab.) “Supusul Celui dăinuitor”. 
+Abdulmubdiy s. (antrop. m., arab.) “Supusul Atoateziditorului”. 
+Abdulmugniy s. (antrop. m., arab.) “Supusul Celui mănos”. 
+Abdulmuğip s. (antrop. m., arab.) “Supusul Celui simŃitor”. 
+Abdulmuhsiy s. (antrop. m., arab.) “Supusul Atoatesocotitorului”. 
+Abdulmukaddim s. (antrop. m., arab.) “Supusul Înaitestrămutătorului”. 
+Abdulmukit s. (antrop. m., arab.) “Supusul SusŃinătorului”. 
+Abdulmuksit s. (antrop. m., arab.) “Supusul Celui chibzuit”. 
+Abdulmuktedir s. (antrop. m., arab.) “Supusul Celui preaputincios”. 
+Abdulmuntakim s. (antrop. m., arab.) “Supusul Osânditorului”. 
+Abdulmusavvir s. (antrop. m., arab.) “Supusul Plămăduitorului”. 
+Abdulmuyit s. (antrop. m., arab.) “Supusul Reînvietorului”. 
+Abdulmuyiz s. (antrop. m., arab.) “Supusul Apreciatorului”. 
+Abdulmúheymin s. (antrop. m., arab.) “Supusul Proteguitorului”. 
+Abdulmúmin s. (antrop. m., arab.) “Supusul Chezaşului”. 
+Abdulnafiy s. (antrop. m., arab.) “Supusul Celui priincios”. 
+Abdulnasîr/Abdulnesir s. (antrop. m., arab.) “Supusul Protectorului”. 
+Abdulnebiy s. (antrop. m., arab.) "Supusul Profetului". 
+Abdulnur s. (antrop. m., arab.) “Supusul Luminii”. 
+Abdulsamet s. (antrop. m., arab.) “Supusul Absolutului”. 
+Abdulselam s. (antrop. m., arab.) “Supusul Păcii”. 
+Abdulşafiy s. (antrop. m., arab.) “Supusul Tămăduitorului”. 
+Abdulşahit s. (antrop. m., arab.) “Supusul Martorului”. 
+Abdulşekúr s. (antrop. m., arab.) “Supusul Celui recunoscător”. 
+Abdultewap s. (antrop. m., arab.) “Supusul Celui îngăduitor şi milostiv”. 
+Abdulvağit s. (antrop. m., arab.) “Supusul Descoperitorului”. 
+Abdulvahit s. (antrop. m., arab.) “Supusul Celui unic”. 
+Abdulvaliy s. (antrop. m., arab.) “Supusul Ocârmuitorului”. 
+Abdulvaris s. (antrop. m., arab.) “Supusul Urmaşului absolut”. 
+Abdulvasiy s. (antrop. m., arab.) “Supusul Celui nemărginit”. 
+Abdulvedút s. (antrop. m., arab.) “Supusul Celui preaiubit şi preaiubitor”. 
+Abdulvehhap s. (antrop. m., arab.) “Supusul Celui ce binecuvântează”. 
+Abdulvekil s. (antrop. m., arab.) “Supusul Celui împuternicit”. 
+Abdulzahir s. (antrop. m., arab.) “Supusul Celui neîndoielnic”. 
+Abdurrahim s. (antrop. m., arab.) “Supusul Celui iertător şi milostiv”. 
+Abdurrahman s. (antrop. m., arab.) “Supusul Celui priitor”. 
+Abdurrakip s. (antrop. m., arab.) “Supusul Priveghetorului”. 
+Abdurrazaak s. (antrop. m., arab.) “Supusul Atoatedătătorului”. 
+Abdurrefiy s. (antrop. m., arab.) “Supusul ÎnălŃătorului”. 
+Abdurreşit s. (antrop. m., arab.) “Supusul Celui ce călăuzeşte pe calea întemeiată”. 
+Abdurrewúf s. (antrop. m., arab.) “Supusul Celui blajin”. 
+aberasiya s. aberaţie. 
+abide s. monument; statuie. // ●kadîm abideler — monumente antice. 
+Abide s. (antrop. f., arab.) "Proslăvitoarea"; "Adoratoarea"; "Roaba"; "Slujitoarea"; "Supusa". 
+abideviy adj. monumental. // ●abideviy donatma — decor monumental. 
+Abit s. (antrop. m., arab.) "Proslăvitorul"; "Adoratorul"; "Robul"; "Slujitorul"; "Supusul" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam). 
+abiy s. (dim., fam.) frăţior mai mare; nene. 
+abîklîk-sabîklîk s. incoerenţă. 
+abîk-sabîk adj. incoerent. 
+abîna-súrúne adv. pe brânci. 
+abîndîrma s. 1. aservire; înrobire; subjugare. 2. împiedicare; îngenunchere. 
+abîndîrmak v.t. 1. a împiedica; a îngenunchea (şi fig.). 2. (fig.) a supune. 
+abîndîruw s. 1. aservire; înrobire; subjugare. 2. împiedicare; îngenunchere. 
+abîngan adj. 1. împiedicat; îngenuncheat; căzut în brânci. 2. (fig.) supus. 
+abînma s. 1. împiedicare. 2. (fig.) supunere. 
+abînmak v.i. 1. a se împiedica; a îngenunchea; a cădea în brânci. 2. (fig.) a se supune. // ● Amet ğúrgende abîna — Amet se împiedică când merge. 
+abînuw s. 1. împiedicare. 2. (fig.) supunere. 
+abîr-şîbîr s. bagatele; mărunţişuri; multe şi mărunte; fleacuri; chiŃibuşuri. 
+abla s. soră mai mare. 
+Abla-Kóy/Abla-Góy s. (topon.) Potârnichea (jud. ConstanŃa). 
+abonament s. abonament. 
+abonamentlí s., adj. abonat. 
+abonat s., adj. abonat. 
+abruy s. stimă; onoare; consideraŃie; respect. 
+abruylî adj. stimat; onorat; respectat. 
+abstrakt adj. abstract. 
+açetilena s. (chim.) acetilenă. 
+açetona s. (chim.) acetonă. 
+açit s. (chim.) acid. 
+açitlemek v.t. (chim.) a acidula. 
+açitleşken adj. (chim.) acid; acidulat. 
+açitleşme s. (chim.) acidulare. 
+açitleşmek v.i. (chim.) a se acidula. 
+açitleştírmek v.t. (chim.) a acidula. 
+açitleşúw s. (chim.) acidulare. 
+açitlí adj. (chim.) acid; acidulat. 
+açitlík s. (chim.) aciditate. 
+ada s. (geogr.) insulă; ostrov. //●takîm adalar — arhipelag. 
+adak s. 1. promisiune; făgăduială; 
+angajament. 2. logodnă. 
+adaklama s. promisiune; făgăduială; angajament. 
+adaklamak v.t. a promite; a făgădui; a se angajament. 
+adaklangan adj. promis; făgăduit; angajat. 
+adaklaw s. promisiune; făgăduială; angajament. 
+adaklawğî adj. promiţător. 
+adaklî adj. promis; făgăduit; angajat. 
+adale s. (anat.) muşchi; musculatură. // ●sîguwğî adale — (anat.) muşchi constrictor. 
+adaleíşí adj. intramuscular. 
+adalelí adj. (anat.) musculos. 
+adalesíz adj. (anat.) fără muşchi; fără musculatură. 
+adalesízlík s. (anat.) lipsă de muşchi; lipsă de musculatură. 
+adalet s. 1. justiŃie. 2. dreptate; 
+echitate; justeţe; imparţialitate; nepărtinire. // ●adalet bakanlîgî / nazirlígí — (pol.) ministerul justiţiei. 
+adaletlí adj. drept; echitabil; just; imparţial; nepărtinitor. 
+adaletsíz adj. injust; inechitabil; nedrept. 
+adaletsízlík s. inechitate; nedreptate. 
+adalgan adj. promis; făgăduit; angajat. 
+adalî adj. insular. 
+adam s. bărbat; om. 
+adamak v.t. a promite; a făgădui; a se angajament. 
+adamakîllî adj. perfect; desăvârşit; ireproşabil. 
+adañ I. adj. exterior; extern; externat; din afară. II. s. exterior; partea din exterior. // ●adañ etmek — a externa. 
+adañda adv. afară; în exterior. 
+adañdan adv. dinspre afară; dinspre exterior. 
+adañdúniyalî adj. 1. extraterestru. 2. nepământean; transcendental. 
+adañdúniyalîk s. transcendentalism. 
+adañgadliy adj. (jur.) extrajudiciar. 
+adañka I. adj. exterior; extern; externat; din afară. II. s. exterior; partea din exterior. // ●a dañ k a e tm e k a externa. 
+adañkaadliy adj. (jur.) extrajudiciar. 
+adañkada adv. afară; în exterior. 
+adañkadan adv. dinspre afară; dinspre exterior. 
+adañkadúniyalî adj. 1. extraterestru. 2. nepământean; transcendental. 
+adañkadúniyalîk s. transcendentalism. 
+adañkaga adv. spre exterior. 
+adañkakanuniy adj. extralegal. 
+adañkameşruw adj. extralegal. 
+adañkanuniy adj. extralegal. 
+adañkatúzel adj. (jur.) extrajudiciar. 
+Adañ-Kúlesí s. (topon.) ("Turnul invadatorilor") Adamclisi (jud. ConstanŃa). 
+adañmeşruw adj. extralegal. 
+adañtúzel adj. (jur.) extrajudiciar. 
+ada-soganî s. (bot.) ceapă-de-mare (lat., Urginea maritima; Scilla maritima). 
+adaşayî s. (bot.) salvie; jale; jaleş; zdraviţă (lat., Salvia of f icinalis). 
+adaşayî, ğabîşkan - s. (bot.) cinsteţ; lăpuş; jale-cleioasă; salvie-cleioasă (lat., Salvia gl utinosa). 
 adaşayî, múslí- s. (bot.) iarba-sfântului-Ioan (lat., Salvia sclarea). 
 adaşayî, túylí- s. (bot.) şerlai (lat., Salvia aethiopis). 
+adaşîk s. (geogr.) insuliţă. 
+adaşkan adj. rătăcit. 
+adaşma s. rătăcire. 
+adaşmak v.i. a se rătăci. 
+adaşuw s. rătăcire. 
+adaw s. promisiune; făgăduială; angajament. 
+adawğî adj. promiŃător.
 `;
 export default rawDictionaryText;
