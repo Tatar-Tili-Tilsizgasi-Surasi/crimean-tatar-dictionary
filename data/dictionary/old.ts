b50357940dd154fde583e75fc70042707b40bbed
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-08-27 17:02:49 +0200

    Delete data/dictionary/old.ts

--- data/dictionary/old.ts
+++
@@ -1,8 +0,0 @@
-const rawDictionaryText = `
-ţal s. (arh.) ţambal.
-ţes v.t. (arh.) a desena; a schiţa.
-ţesmek v.t. (arh.) a desena.
-ţín s. (arh.) rang; cin.
-Bulgarístan s. (topon.) Bulgaria.
-`;
-export default rawDictionaryText;
