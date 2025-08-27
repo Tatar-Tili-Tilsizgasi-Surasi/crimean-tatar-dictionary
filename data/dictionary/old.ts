70d6ef8767fb5748985de5d501725ec632ca7aa9
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-08-27 16:18:53 +0200

    Add files via upload

---
+++ data/dictionary/old.ts
@@ -0,0 +1,7 @@
+const rawDictionaryText = `
+ţal s. (arh.) ţambal.
+ţes v.t. (arh.) a desena; a schiţa.
+ţesmek v.t. (arh.) a desena.
+ţín s. (arh.) rang; cin.
+`;
+export default rawDictionaryText;
