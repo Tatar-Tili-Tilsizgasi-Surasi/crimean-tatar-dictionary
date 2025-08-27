3375c480dc325a817655956cb0bb163b56f06ae0
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-08-27 10:10:35 +0200

    Update w.ts

--- data/dictionary/w.ts
+++ data/dictionary/w.ts
@@ -22,5 +22,120 @@ wakîldamak v.i. a măcăi; a măcăni; a face mac-mac; a orăcăi.
 wakîltî s. măcăit; măcănit; orăcăit. 
 wakîp s. donaŃie; fundaŃie. 
 wakît s. 1. timp; vreme. 2. interval; perioadă; răstimp. 3. prilej; ocazie. 4. sezon; anotimp. 5. stare materială; posibilităŃi materiale. 6. epocă; ev; eră. // ●wakît kazanmak — a câştiga timp. ●wakîtî bolmak — a avea timp. ●wakîtî kelmek — a i se împlini termenul; a-i sosi momentul; a i se împlini sorocul. ●wakîtîn geşírmek — a-şi petrece timpul. ●wakîtîn óttírmek — a-şi omorî timpul. ●wakîtîn şaşmak — a-i răpi cuiva timpul. ●wakîtîn totîrmak — a-şi umple timpul. // ●başka wakît — altă dată. ●boş/asude wakît — timp liber. ●eñ kîzgîn wakît — punct culminant. ●íşím bar, wakîtîm yok — sunt ocupat, n-am timp. ●keş wakît — oră târzie. ●kîzgîn wakît — ore de vârf. ●ne wakît? — când?; ●ódeme wakîtî — (fin.) scadenŃă. ●wakît kîtlîgî — criză de timp. ●wakîtîm yok — nu am timp. ●wakîtîñ bolsa — dacă ai timp. ●wakîtîndan burun — prematur. ●wakîtnîñ insanlarî — oamenii timpului. ●wakîtnîñ kóbí — cea mai mare parte a timpului. ●yakîn wakîtka! — pe curând!;
+wakît-hál s. situaŃie materială; posibilităŃi materiale. // ●wakîtî-hálí yerínde — înstărit; cu mari posibilităŃi materiale. 
+wakîtîgeşken adj. învechit; depăşit; demodat; bătrânesc; caduc; desuet; perimat. 
+wakîtînda adv. 1. în timpul; pe vremea când; exact când. 2. la momentul potrivit. // ●íş wakîtînda otîrmak yok — în timpul lucrului nu se stă. ●íşín wakîtînda yapkan — punctual. ●wakîtînda barağakmîz — vom ajunge la timp. 
+wakîtîndan adv. din timp; din timpul; de pe vremea când. 
+wakîtlar s., pl. împrejurare; circumstanŃă; context; conjunctură. 
+wakîtlî I. adj. cu situaŃie materială; cu posibilităŃi materiale; avut; bogat. II. adv. la momentul potrivit; din timp; devreme. 
+wakîtlî-wakîtînda adv. 1. la momentul potrivit; din timp; devreme. 2. bine făcut. 
+wakîtlî-wakîtsîz adv. 1. în momente nepotrivite. 2. chiar şi în criză de timp. // ●wakîtlî-wakîtsîz kele — vine în momente nepotrivite. 
+wakîtsîz I. adj. fără timp. II. adv. în momente nepotrivite. 
+wakîtsîzlîk s. criză de timp. 
+wakîtta adv. la momentul; în momentul. // ●bonday wakîtta — pe asemenea vreme. ●uygun wakîtta — la momentul oportun. 
+wakîttan adv. din momentul; din vremea; în avans; înainte de. 
+wakîttan-wakîtka adv. din când în când; uneori. 
+wakît-wakît adv. din când în când; uneori. 
+waklîk s. mărunŃiş; bani mărunŃi. 
+wakpetílgen adj. 1. donat; dăruit. 2. consacrat; a dedicat. 
+wakpetílmek v.i. 1. a se dona; a se dărui. 2. a se consacra; a se dedica. 
+wakpetken s. donator. 
+wakpetmek v.t. 1. a consacra; a dedica. 2. a dona; a dărui. 
+wakpetúw s. dedicaŃie; donaŃie. 
+wakpetúwğí s. donator. 
+wak-wak interj. mac!; oac!; 
+walla(h) interj. zău!; pe legea mea!; parol!; 
+walla(h)-billa(h) interj. zău!; pe legea mea!; parol!; 
+wañgîrîşmak v.i. a se tângui laolaltă; a boci laolaltă; a jeli laolaltă. 
+wañgîrma s. tânguire; bocire; jelire. 
+wañgîrmak v.i. a se tângui; a boci; a jeli. 
+wañgîruwğî s. bocitoare. 
+wat s. (fiz.) watt. 
+watan s. patrie; Ńară. // ●watan súygúsí — dragoste de patrie. ●watanga ihanet — trădare de patrie. 
+watandaş s. compatriot; cetăŃean; concetăŃean. // ●watandaş hakklarî — (jur.) drepturi civile. 
+watandaşlîk s. cetăŃenie. 
+wataniy adj. patriotic. // ●wataniy tuygular — sentimente patriotice. 
+watanperwer s. patriot. 
+watanperwerlík s. patriotism. 
+watansîz s. apatrid. 
+watansúygen s. patriot. 
+watansúyme s. patriotism. 
+watanşúnas s. patriot. 
+watanşúnaslîk s. patriotism. 
+watîlgan adj. mărunŃit; zdrobit; sfărâmicios. 
+watîlmak v.i. a se sfărâma; a se fărâmiŃa; a se mărunŃi. 
+watma s. sfărâmare; fărâmiŃare; mărunŃire. 
+watmak v.t. a sfărâma; a fărâmiŃa; a mărunŃi. 
+watmetre s. wattmetru. 
+watólşer s. wattmetru. 
+watsaniye s. (fiz.) wattsecundă. 
+watuwğî adj. zdrobitor. 
+watuwlî adj. sfărâmat; fărâmiŃat; mărunŃit; zdrobit. 
+way interj. vai!; oh!; aoleu!; // ●way başîma! — vai de capul meu!; ●way ğañgîzlarga — vai de omul singur. ●way ğeñílgenlerge — vai de învinşi. ●way neniyím/anayîm! — vai, măicuŃă!; 
+way-way I. interj. vai!; oh!; vai-vai!; II. s. vaiet. 
+wazife s. 1. datorie; îndatorire; obligaŃie. 2. misiune; sarcină. //●wazifesín deñíştírmek — a-i schimba funcŃia; a-l permuta. ●wazifesínden şîgarmak/almak — a destitui pe cineva. ●wazifesíne salmak — a pune în funcŃie pe cineva; a-l învesti; a-l însărcina. // ●riyaziyet wazifesí — tema la matematică. ●siyasiy wazife — legaŃie; misiune diplomatică. ●wazife başînda — în exerciŃiul funcŃiunii. ●wazifeníñ yerine akelínmesí — îndeplinirea misiunii. 
+wazifeberílgen adj. însărcinat; numit; desemnat. 
+wazifeberúw s. însărcinare; numire; desemnare. 
+wazifedeñíştírgen adj. permutat. 
+wazifelendírílgen adj. acreditat; delegat; împuternicit; mandatat. 
+wazifelendírme s. acreditare; delegare; împuternicire; mandatare. 
+wazifelendírmek v.t. a acredita; a delega; a împuternici; a mandata. 
+wazifelí adj. însărcinat; împuternicit; mandatat; acreditat. // ●wazifelí şahîs — funcŃionar. 
+wazifename s. împuternicire; mandat. 
+wazifesíndenalîngan adj. revocat; destituit; concediat; demis. 
+wazifesíndenaluw s. revocare; destituire; concediere; demitere. 
+wazifesíndenatîlgan adj. revocat; destituit; concediat; demis. 
+wazifesíndenatuw s. revocare; destituire; concediere; demitere. 
+wazifesíndentartîlgan adj. revocat; destituit; concediat; demis. 
+wazifesíndentartuw s. revocare; destituire; concediere; demitere. 
+wazifesínesalîngan adj. abilitat; însărcinat; înscăunat; întronat; învestit. 
+wazifesínesaluw s. abilitare; însărcinare; înscăunare; întronare; învestitură. 
+wazifesíz adj. fără sarcini; fără ocupaŃie; fără griji. 
+wazifesízlík s. lipsă de sarcini; lipsă de ocupaŃie; lipsă de griji. 
+wagamlaw s. răscoacere. 
+wakîldaw s. măcăit; măcănit; orăcăit. 
+wakşeşeklí-ğalba s. (bot.) lumânărică; coada-lupului (lat., Verbascum thapsus). 
+wakşeşeklí-sîyîrkuyrugî s. (bot.) lumânărică; coada-lupului (lat., Verbascum thapsus). 
+wañgîruw s. tânguire; bocire; jelire. 
+watuw s. sfărâmare; fărâmiŃare; mărunŃire. 
+we conj. şi. // ●eglenúw we díñlenme koşîllarî — condiŃii de distracŃie şi odihnă. ●kîrîm tatar tílí we edebiyatî — limba şi literatura tătară crimeană. ●mahsús we namahsús — abia perceptibil. ●meğburiy we meğğaniy — obligatoriu şi gratuit. ●we sayire — etcetera; şi aşa mai departe. 
+wekíl s. 1. împuternicit; mandatar; reprezentant. 2. avocat. 3. ministru. 4. (jur.) tutore; curator. 
+wekíllík s. (jur.) tutelă; curatelă. 
+wekíllíktenboşangan adj. (jur.) emancipat. 
+wekíllíktenboşatma s. (jur.) emancipare. 
+welew conj. chiar dacă; deşi. 
+welí s. 1. (relig.) sfânt; sacru. 2. (jur.) tutore; curator. 
+welílík s. 1. (relig.) sfinŃenie. 2. (jur.) tutelă; curatelă. 
+wepat s. deces; moarte. // ●wepat etmek/bolmak — a deceda; a muri. 
+wepatbolgan adj. decedat; defunct. 
+wepatlî adj. decedat; mort. 
+wesselam interj. suficient!; îndeajuns!; ajunge!; asta-i tot!; 
+western s. western. 
+White'níñ-sayragî s. (orn.) sturz auriu (lat., Zoothera dauma). 
+widiya s. (tehn.) widia. 
+wigwam s. wigwam. 
+windsurf s. windsurf. 
+windsurfşî s. surfist. 
+windsurfşuluk s. surfist. 
+wiskiy s. whisky. 
+wok s. wok. 
+wolfram I. adj. (chim.) wolframic. II. s. (chim.) wolfram. 
+wolframlî adj. (chim.) wolframic. 
+wurdumtuymaz adj. 1. nesimŃit; lipsit de bun-simŃ. 2. insensibil. 
+wurdumtuymazlîk s. insensibilitate; nesimŃire. 
+wurdurmak v.t. 1. a determina să lovească; a determina să izbească; a determina să rănească. 2. a determina să împuşte; a determina să ucidă. 3. a determina să bată; a determina să ciocănească. 
+wurgan s. (mat.) înmulŃitor; factor. 
+wurgan-ğîkkan adj. 1. bătăios; combativ; certăreŃ. 2. temerar; îndrăzneŃ; cutezător; neînfricat; aventuros. 
+wurgî s. 1. (lingv.) accent. 2. (muz.) ton; tonalitate; intonaŃie. 
+wurgîsaluw s. accentuare; evidenŃiere; reliefare; marcare. 
+wurgulama s. accentuare; exclamare. 
+wurgulamak v.t. a accentua; a exclama. // ●wurgulap bíldírmek — a preciza; a specifica. ●wurgulap kîşkîrmak — a exclama. 
+wurgulangan adj. (fon.) accentuat; tonic. 
+wurgulanmak v.i. a se accentua. 
+wurgulap-bíldírílgen adj. precizat; specificat. 
+wurgulap-bíldírúw s. precizare; specificare. 
+wurgulay-wurgulay adv. accentuat; răspicat. 
+wurgulî adj. (fon.) accentuat; tonic. 
+wurgulîkîşkîrma s. exclamare; exclamaŃie.
 `;
 export default rawDictionaryText;
