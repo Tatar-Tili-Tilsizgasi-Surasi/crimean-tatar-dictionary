2904a244a09fb9b6ba520dc332c4e815c5770ee4
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-08-27 09:39:23 +0200

    Update w.ts

--- data/dictionary/w.ts
+++ data/dictionary/w.ts
@@ -1,2 +1,26 @@
-const rawDictionaryText = ``;
+const rawDictionaryText = `
+wagam adj. răscopt. 
+wagamlama s. răscoacere. 
+wagamlamak v.i. a se răscoace. 
+wahşet s. sălbăticie; cruzime; brutalitate. 
+wahşiy adj. 1. sălbatic; crud; violent; brutal; neîmblânzit. 2. (d. animale, păsări) sălbatic. // ●wahşiy dawa tutmak — a linşa. 
+wahşiy-ağîmasîz s., adj. sadic. 
+wahşiybogaz s., adj. mâncău; mâncăcios; gurmand; vorace. 
+wahşiybogazlîk s. voracitate. 
+wahşiyğe adv. sălbatic; crud; violent; brutal. 
+wahşiyleşken adj. sălbăticit; abrutizat. 
+wahşiyleşme s. sălbăticire; abrutizare. 
+wahşiyleşmek v.i. a se sălbătici; a se abrutiza. 
+wahşiyleştírmek v.t. a sălbătici; a abrutiza. 
+wahşiylík s. sălbăticie; cruzime; violenŃă; brutalitate; bestialitate. 
+wahşiylík-ağîmasîzlîk s. sadism. 
+wahşiytabiyat s. sălbăticie; cruzime; violenŃă; brutalitate; bestialitate. 
+wahşiytabiyatlî adj. sălbatic; crud; violent; brutal; bestial. 
+wak adj. mărunt; mic. // ●wak ğísím — corpuscul. ●wak para — bani mărunŃi; mărunŃiş. 
+wakîldama s. măcăit; măcănit; orăcăit. 
+wakîldamak v.i. a măcăi; a măcăni; a face mac-mac; a orăcăi. 
+wakîltî s. măcăit; măcănit; orăcăit. 
+wakîp s. donaŃie; fundaŃie. 
+wakît s. 1. timp; vreme. 2. interval; perioadă; răstimp. 3. prilej; ocazie. 4. sezon; anotimp. 5. stare materială; posibilităŃi materiale. 6. epocă; ev; eră. // ●wakît kazanmak — a câştiga timp. ●wakîtî bolmak — a avea timp. ●wakîtî kelmek — a i se împlini termenul; a-i sosi momentul; a i se împlini sorocul. ●wakîtîn geşírmek — a-şi petrece timpul. ●wakîtîn óttírmek — a-şi omorî timpul. ●wakîtîn şaşmak — a-i răpi cuiva timpul. ●wakîtîn totîrmak — a-şi umple timpul. // ●başka wakît — altă dată. ●boş/asude wakît — timp liber. ●eñ kîzgîn wakît — punct culminant. ●íşím bar, wakîtîm yok — sunt ocupat, n-am timp. ●keş wakît — oră târzie. ●kîzgîn wakît — ore de vârf. ●ne wakît? — când?; ●ódeme wakîtî — (fin.) scadenŃă. ●wakît kîtlîgî — criză de timp. ●wakîtîm yok — nu am timp. ●wakîtîñ bolsa — dacă ai timp. ●wakîtîndan burun — prematur. ●wakîtnîñ insanlarî — oamenii timpului. ●wakîtnîñ kóbí — cea mai mare parte a timpului. ●yakîn wakîtka! — pe curând!;
+`;
 export default rawDictionaryText;
