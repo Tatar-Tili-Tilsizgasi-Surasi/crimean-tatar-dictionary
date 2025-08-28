72203655bcf8be0143e7cd2c4937db9ec6833f5a
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-08-28 12:38:27 +0200

    Update v.ts

--- data/dictionary/v.ts
+++ data/dictionary/v.ts
@@ -31,5 +31,134 @@ Vahide s. (antrop. f., arab.) “Cea fără pereche”.
 vahim adj. serios; grav; periculos. 
 vahit I. adj. unic; singular. II. num. (arab.) unu. 
 Vahit, El- s. (relig., arab.) "Cel unic"; “Cel atotcuprinzător şi indivizibil”; “Cel a cărui personalitate nu are pereche”; “Cel ale cărui însuşiri şi nume sunt neasemuite”; “Cel ale cărui fapte sunt incomparabile” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah).
+vahiy adj. neserios; nefondat; absurd. 
+vahşet s. sălbăticie; cruzime; brutalitate. 
+vahşiy adj. sălbatic. 
+vahşiylík s. sălbăticie. 
+vaka s. întâmplare; eveniment; faptă. 
+vakaa adv. într-adevăr; realmente; efectiv. 
+vakar s. demnitate; seriozitate; gravitate. 
+vakarlî adj. demn; serios; grav. 
+vakarsîz adj. nedemn; neserios. 
+vakayiyname s. 1. cronică; letopiseŃ. 2. anale. 
+vakayiynameğí s. cronicar; analist. 
+vakfetílgen adj. 1. donat; dăruit. 2. consacrat; a dedicat. 
+vakfetílmek v.i. 1. a se dona; a se dărui. 2. a se consacra; a se dedica. 
+vakfetken s. donator. 
+vakfetmek v.t. 1. a dona; a dărui (bunuri). 2. a consacra; a dedica. 
+vakfetúw s. dedicaŃie; donaŃie. 
+vakfetúwğí s. donator. 
+vakiy adj. întâmplat; petrecut; care a avut loc. 
+vakîf s. donaŃie; fundaŃie. 
+vakîfname s. act de donaŃie. 
+vakum s. (fiz.) vacuum; vid. 
+vakur adj. serios; aşezat; impozant. 
+valensa s. (chim.) valenŃă. 
+valide s. (ist.) mamă. 
+Valide s. (antrop. f., arab.) “Cea nou-născută”. 
+Valit s. (antrop. m., arab.) “Pruncul”. 
+valiy s. 1. (adm.) prefect; guvernator. 2. conducător; cârmuitor; ocârmuitor; diriguitor. 
+Valiy, El- s. (relig., arab.) "Ocârmuitorul"; “Cel ce diriguieşte întregul univers” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah). 
+valiza s. valiză. 
+vals s. vals (dansul şi muzica). //●vals oynamak — a valsa. 
+valva s. (tehn.) ventil; valvă. 
+vampir s. vampir. 
+vanadiyum s. (chim.) vanadiu. 
+vandal s. vandal (şi fig.). 
+vandalism s. vandalism. 
+vaniliyalî adj. cu vanilie; vanilat. //●vanilyalî şokolat — a ciocolată cu vanilie. 
+Vanuwat s. (topon.) Vanuatu. 
+varaka s. hârtie; document. 
+varakalî s., adj. documentar. 
+vardiya s. 1. (nav.) cart. 2. schimb; serviciu. 
+varidat s. (econ.) venit. 
+varik s. (med.) varice. 
+varis s. moştenitor; succesor; legatar. 
+Varis, El- s. (relig., arab.) "Urmaşul absolut"; “Stăpânitorul veşnic şi moştenitorul de drept al universului” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah). 
+variyanta s. variantă; alternativă. 
+variyasiya s. (muz.) variaŃiune. 
+variyete s. varieteu; varietăŃi. //●v a r i y e t e t em s i l í spectacol de varietăŃi. 
+varoş s. cartier mărginaş; periferie. 
+varta s. pericol; situaŃie primejdioasă. // ●v a r t an î a t l a tm a k a evita pericolul. 
+vasat s. mijloc; centru. 
+vasatiy adj. mijlociu; intermediar. 
+vaselina s. (chim.) vaselină. 
+vasiy s. (jur.) tutore; curator. 
+Vasiy, El- s. (relig., arab.) "Cel nemărginit"; “Cel ce nu cunoaşte margini în bunătatea şi puterea Sa” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah). 
+vasiyet s. testament. // ●v a s i y e t e tm e k a lăsa testament. 
+vasiyetiy adj. testamentar. //●v a s i y e t i y i ğ r a m em u r (jur.) executor testamentar. 
+vasiyetname s. (jur.) act testamentar; testament. // ●v a s i y e t n am eg e k ó r e testamentar. 
+vasiylík s. (jur.) tutelă; curatelă. 
+vasîf s. însuşire; calitate; caracteristică. 
+vasîflama s. calificare. 
+vasîflamak v.t. a caracteriza; a califica. 
+vasîflangan adj. calificat. 
+vasîflanmagan adj. necalificat. 
+vasîflanmak v.i. a se caracteriza; a fi calificat. 
+vasîflawğî adj. calificativ. 
+vasîl s. sosire; venire; ajungere. //●v a s î l b o lm a k a sosi; a veni; a ajunge. 
+Vasîl s. (antrop. m., arab.) "Mijlocitorul divinităŃii" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam). 
+vasîllî adj. sosit; venit; ajuns. 
+vasîta s. 1. mijloc; cale. 2. intermediu; intermediere; mijlocire. 3. intermediar; mijlocitor; vehicul. // ●h a w a na k l i y a t v a s î t a s î mijloc de transport aerian. ●m o t o r l î v a s î t a autovehicul. ●n a k i l v a s î t a s î mijloc de transport. ●ú r e t ím / i s t i h s a l v a s î t alarî mijloace de producŃie. ●vasîtasî man — prin intermediul…; 
+vasîtağî s. intermediar; mijlocitor; misit. 
+vasîtalî adj. indirect. // ●vasîtalî bergí — impozit indirect. 
+vasîtasîz adj. direct; nemijlocit. //●vasîtasîz bergí — impozit direct. 
+vatan s. patrie; Ńară. 
+vatandaş s. compatriot; cetăŃean; concetăŃean. 
+vatandaşlîk s. cetăŃenie. 
+vataniy adj. patriotic. 
+vatanperwer s. patriot. 
+vatelina s. (text.) vatelină. 
+Vatikan s. (topon.) Vatican. 
+vatman s. vatman. 
+vayiz s. 1. (relig.) povăŃuitor; predicator; propovăduitor; binevestitor. 2. povăŃuitor; călăuzitor; îndrumător; ghid. 
+Vayiz s. (antrop. m., arab.) "PovăŃuitorul" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam). 
+vayizğí s. (relig., la musulmani) predicator. 
+vayizlík s. (relig.) povăŃuire; povaŃă; predicare; predică; propovăduire; binevestire. 
+vayîm s. sentiment; sensibilitate; impresie. 
+vayîmsîzlîk-tuygusuzluk s. insensibilitate. 
+vayîmsîz-tuygusuz adj. insensibil; indiferent. 
+vayîz s. (relig.) predică. 
+vaza s. vază; glastră. // ●kristal vaza — vază de cristal. 
+vazife s. 1. datorie; îndatorire; obligaŃie. 2. misiune; sarcină. 
+vaziyet s. 1. poziŃie; aşezare; situaŃie. 2. situaŃie; stare; împrejurare; circumstanŃă. // ●şîgîlmaz vaziyetke ketírmek — a aduce într-o situaŃie fără ieşire; a nu lăsa nicio portiŃă de scăpare; a pune în dificultate. // ●ewelkí vaziyette — în starea anterioară. ●sáde bo vaziyet úşún — ad hoc; numai pentru această situaŃie. ●şîgîlmaz vaziyet — impas. ●zaruriy vaziyet — situaŃie critică; urgenŃă. 
+vaziyet-í zaruret iz.prsn. situaŃie critică; urgenŃă. 
+vazîh adj. clar; limpede; evident. 
+vákîf adj. ştiutor; cunoscător; competent. 
+váris s. moştenire; urmaş. 
+vaniliya s. (bot.) vanilie (lat., Vanilla planifolia). 
+vasîflaw s. calificare. 
+veba s. (med.) holeră. 
+vebalî adj. (med.) holeric. 
+vebaotî s. (bot.) captalan; rădăcina-ciumei (lat., Petasites officinalis; Petasites hybridus; Tussilago petasites). 
+veda s. rămas bun; adio. // ●veda etmek — a-şi lua rămas bun; a-şi lua adio. 
+vedalaşmak v.i. a-şi lua rămas bun; a-şi lua adio. 
+Vedat s. (antrop. m., arab.) “Prietenie”. 
+vedre s. vadră. 
+vedreleme s. măsurare cu vadra. 
+vedrelemek v.t. a măsura cu vadra. 
+vedrelengen adj. măsurat cu vadra. 
+vedre-vedre adv. cu vadra; vedre de. 
+Vedút/Wedút, El- s. (relig., arab.) "Cel preaiubit şi preaiubitor"; Cel ce îşi iubeşte nespus supuşii şi cel mai demn de iubire” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah). 
+vefa s. 1. constanŃă; statornicie. 2. devotament; ataşament; fidelitate; îndeplinirea promisiunii. 
+vefakáar adj. 1. constant; statornic. 2. devotat; ataşat; fidel. 
+vefasîz adj. inconstant; nestatornic; infidel. 
+vefasîzlîk s. inconstanŃă; nestatornicie. 
+vefat s. deces; moarte. 
+Vefiy s. (antrop. m., arab.) "Cel ce îşi îndeplineşte promisiunile" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam). 
+Vefiye s. (antrop. f., arab.) “Cea care îşi îndeplineşte promisiunile”. 
+veğetariyan s., adj. vegetarian. 
+veğibe s. obligaŃie; îndatorire. 
+veğit s. extaz; beatitudine; transă. 
+veğitlí adj. extaziat. 
+veğize s. 1. maximă; aforism; cugetare. 2. sentinŃă. 
+veğizelí adj. sentenŃios. 
+Vehbiye s. (antrop. f., arab.) “Cea care binecuvântează”. 
+Vehhap, El- s. (relig., arab.) "Cel ce binecuvântează"; “Cel ce revarsă graŃia divină asupra tuturor vieŃuitoarelor” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah). 
+vehim s. închipuire; iluzie; plăsmuire. 
+vehimğí s. născocitor; inventator. 
+vehimlí adj. închipuit; plăsmuit; născocit. 
+vekálet s. 1. împuternicire; autorizare; autorizaŃie; mandat. 2. (arh.) minister. 
+vekáletiye s. împuternicire; autorizare; autorizaŃie; mandat.
 `;
 export default rawDictionaryText;
