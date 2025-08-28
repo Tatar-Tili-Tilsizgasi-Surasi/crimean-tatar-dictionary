907ce9bedf6fc4fadd526853e958b6c94520d527
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-08-28 12:26:23 +0200

    Update v.ts

--- data/dictionary/v.ts
+++ data/dictionary/v.ts
@@ -1,2 +1,35 @@
-const rawDictionaryText = ``;
+const rawDictionaryText = `
+vaat s. promisiune; făgăduială. //●sízge bírşiy vaat et-almam — nu vă pot promite nimic. 
+vaatetmek v.t. a promite; a făgădui. 
+vaatetúwğí adj. promiŃător. 
+vaattetme s. promisiune; făgăduială. 
+vaaz s. (relig.) povăŃuire; povaŃă; predicare; predică; propovăduire; binevestire. 
+vaazetmek v.i. (relig.) a povăŃui; a predica; a propovădui; a binevesti. 
+vaazetúw s. (relig.) povăŃuire; predicare; propovăduire; binevestire. 
+vaazğî s. povăŃuitor; predicator; propovăduitor; binevestitor. 
+vaazlîk s. povăŃuire; povaŃă; predicare; predică; propovăduire; binevestire. 
+vabeste adj. legat; dependent. //●vabeste bolmak — a depinde de. 
+vade s. termen; scadenŃă. // ●vadesí kelmek — a i se împlini termenul; a i se împlini sorocul; a muri. ●vadesín uzatmak — a-i prelungi termenul. //●uzun vadelí ikraz — credit pe termen lung. 
+vadesíkelgen adj. 1. căruia i-a venit sorocul; mort. 2. expirat; nul. 
+vadesítolgan adj. 1. căruia i s-a împlinit sorocul; mort. 2. expirat; nul. 
+vadesíuzagan adj. amânat; păsuit. 
+vadesíz adj. fără termen; sine die. 
+vadeuzatmasî s. amânare; păsuire. 
+vadiy s. 1. (geogr.) vale; vâlcea. 2. (fig.) sens; înŃeles. 
+vagon s. vagon. // ●çisterna vagonî — vagon cisternă. ●ğataklî vagon — vagon de dormit. ●lokantalî/restawurantlî vagon — vagon restaurant. ●mal vagonî — vagon de marfă. ●tanklî vagon — vagon cisternă. ●úşúnğí vagon — al treilea vagon. 
+vagonet s. vagonet. 
+Vağide s. (antrop. f., arab.) “Cea care descoperă”. 
+vağina s. (anat.) vagin. 
+vağip adj. necesar; indispensabil. 
+vağiplík s. necesitate; trebuinŃă. 
+vağit s. găsitor; descoperitor. 
+Vağit, El- s. (relig., arab.) "Descoperitorul"; “Cel ce găseşte orice şi oricând” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah). 
+vah interj. vai!; 
+vaha s. (geogr.) oază; limbă de pământ. 
+vahamet s. seriozitate; gravitate; periculozitate. 
+Vahide s. (antrop. f., arab.) “Cea fără pereche”. 
+vahim adj. serios; grav; periculos. 
+vahit I. adj. unic; singular. II. num. (arab.) unu. 
+Vahit, El- s. (relig., arab.) "Cel unic"; “Cel atotcuprinzător şi indivizibil”; “Cel a cărui personalitate nu are pereche”; “Cel ale cărui însuşiri şi nume sunt neasemuite”; “Cel ale cărui fapte sunt incomparabile” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah).
+`;
 export default rawDictionaryText;
