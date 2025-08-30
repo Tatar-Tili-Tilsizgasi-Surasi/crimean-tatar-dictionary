1cda5205a2b68eb0e91afae3602c35dacf716dde
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-08-30 13:42:33 +0200

    Update r.ts

--- data/dictionary/r.ts
+++ data/dictionary/r.ts
@@ -1,2 +1,92 @@
-const rawDictionaryText = ``;
+const rawDictionaryText = `
+raát I. adj. 1. liniştit; odihnit; relaxat. 2. comod; confortabil. II. s. linişte; tihnă; odihnă; relaxare. // ●raát bolmak  — a se linişti; a se odihni. ●raátí kalmamak — a nu mai avea linişte/tihnă/pace. ●raátín bîzmak — a strica pacea cuiva; a-i strica liniştea. 
+raátetken adj. reconfortant. 
+raátkana s. closet; toaletă. 
+raátlendírmek v.t. a odihni; a linişti; a relaxa. 
+raátlendírúwğí s., adj. odihnitor; liniştitor; relaxant; calmant. 
+raátlenme s. odihnă; relaxare. 
+raátlenmek v.i. a se odihni; a se linişti; a se relaxa. // ●begárúw raátlendím — m-am odihnit foarte bine. ●bíraz raátleneğek bolaman — vreau să mă odihnesc puŃin. 
+raátlígíbîzîlgan adj. stânjenit; stingherit; incomodat; jenat; încurcat; neliniştit; alarmat. 
+raátlígínbîzmaz adj. imperturbabil. 
+raátlík s. linişte; tihnă; odihnă. //●raátlígín bîzmak — a strica pacea cuiva; a-i strica liniştea. // ●raátlík yerí — staŃiune. 
+raát-raát adv. 1. liniştit; calm. 2. liber; descătuşat. 
+raátsíz adj. 1. neliniştit; frământat; tulburat. 2. bolnav; suferind. 3. incomod; neconfortabil. //●búyrekten raátsízmen — sunt bolnav de rinichi. ●heş raátsíz etmedíñíz — nu m-aŃi deranjat deloc. ●karağígerden raátsízmen — sunt bolnav de ficat. ●raátsíz bolmañîz! — nu vă deranjaŃi!; ●sízní raátsíz ettím — v-am deranjat. 
+raátsízetken adj. stânjenitor; stingheritor; incomod; jenant; neliniştitor; alarmant. 
+raátsízlendírmek v.t. 1. a îmbolnăvi. 2. a nelinişti; a rământa; a tulbura. 
+raátsízlenmek v.i. 1. a se nelinişti; a se frământa; a se tulbura. 2. a se îmbolnăvi. 
+raátsízlík s. 1. nelinişte; frământare; tulburare. 2. boală; indispoziŃie; proastă dispoziŃie. 
+raátşe adv. 1. liniştit; calm. 2. liber; descătuşat. 
+rabbaniy adj. divin; sacru; dumnezeiesc. 
+Rabiye s. (antrop. f., arab.) “Împrimăvărarea”. 
+rabît s. 1. unire; legare; împreunare. 2. (gram.) conjuncŃie. 
+rabîta s. 1. legătură; relaŃie; conexiune. 2. coerenŃă. 3. (gram.) copulă. 
+rabîtağî s. radiotelegrafist. 
+rabîtağîlîk s. radiotelegrafie. 
+rabîtalandîrma s. legare; conectare. 
+rabîtalandîrmak v.t. a lega; a conecta. 
+rabîtalandîruw s. legare; conectare. 
+rabîtalangan adj. legat; conectat. 
+rabîtalanmak v.i. a se lega; a se conecta. 
+rabîtal î adj. 1. legat; strâns; conectat. 2. coerent. 
+rabîtasîz adj. incoerent. 
+rabîtasîzlîk s. incoerenŃă. 
+rabîtlî adj. 1. (gram.) cu conjuncŃie. 2. unit; legat; împreunat. 
+rabîtşî s. unificator; montator. 
+radar s. (tel.) radiolocaŃie; radar. 
+radargakarşî adj. antiradar. 
+Radde'níñ-ğelbegesí s. (orn.) pitulice măslinie (lat., Phylloscopus schwarzi). 
+Radde'níñ-sarîkaşî s. (orn.) pitulice măslinie (lat., Phylloscopus schwarzi). 
+radikal s., adj. radical. 
+radikalğî s. radicalist. 
+radikalism s. radicalism. 
+radikalist s. radicalist. 
+radikallaşkan adj. radicalizat. 
+radikallaşma s. radicalizare. 
+radikallaşmak v.i. a se radicaliza. 
+radikallaştîrmak v.t. a radicaliza. 
+radikallaştîruw s. radicalizare. 
+radikallaşuw s. radicalizare. 
+radikallîk s. radicalism. 
+radiyasiya s. radiaŃie. 
+radiyasiyalî adj. iradiat; iradiant. 
+radiyator s. radiator. 
+radiyo s. radio. 
+radiyoaktif adj. (fiz.) radioactiv. 
+radiyoaktifleşmek v.i. (fiz.) a deveni radioactiv. 
+radiyoaktiflík s. (fiz.) radioactivitate. 
+radiyoaluwğî s. radioreceptor. 
+radiyoastronom s. radioastronom. 
+radiyoastronomik adj. radioastronomic. 
+radiyoastronomiya s. radioastronomie. 
+radiyoastronomiyağî s. radioastronom. 
+radiyoastronomiyalî adj. radioastronomic. 
+radiyoberímí s. radiodifuziune. 
+radiyoberúwğí s. 1. releu. 2. radioemiŃător. 
+radiyodaberílgen adj. radiodifuzat. 
+radiyodağayîlgan adj. radiodifuzat. 
+radiyodalga s. (fiz.) undă radio. 
+radiyodulkun s. (fiz.) undă radio. 
+radiyofaal adj. (fiz.) radioactiv. 
+radiyofaaliyet s. (fiz.) radioactivitate. 
+radiyofaallaşmak v.i. (fiz.) a deveni radioactiv. 
+radiyofaallîk s. (fiz.) radioactivitate. 
+radiyofelekiyat s. radioastronomie. 
+radiyofelekiyatlî adj. radioastronomic. 
+radiyofelekiyatşî s. radioastronom. 
+radiyofonik adj. radiofonic. 
+radiyofoniya s. radiofonie. 
+radiyofoniyalî adj. radiofonic. 
+radiyogázáta s. radiojurnal. 
+radiyografiya s. radiografie. 
+radiyograma s. radiogramă. 
+radiyoğayîmî s. radiodifuziune. 
+radiyoğayîn s. radiotransmisie; emisiune radio. 
+radiyoğayînlama s. radiodifuzare; radiodifuziune. 
+radiyoğayînlamak v.t. a radiodifuza. 
+radiyoğayînlangan adj. radiodifuzat. 
+radiyoğayînlanmak v.i. a se radiodifuza. 
+radiyoğayînlaw s. radiodifuzare; radiodifuziune. 
+radiyoğayînlawğî s. radioemiŃător. 
+radiyoğayînlî adj. radiodifuzat.
+`;
 export default rawDictionaryText;
