f88bb84d8b21f389afd6d0b27b1c5a966d5862be
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-08-31 10:30:29 +0200

    Update r.ts

--- data/dictionary/r.ts
+++ data/dictionary/r.ts
@@ -88,5 +88,151 @@ radiyoğayînlanmak v.i. a se radiodifuza.
 radiyoğayînlaw s. radiodifuzare; radiodifuziune. 
 radiyoğayînlawğî s. radioemiŃător. 
 radiyoğayînlî adj. radiodifuzat.
+radiyoğayuwğî s. releu. 
+radiyoheyet s. radioastronomie. 
+radiyoheyetlí adj. radioastronomic. 
+radiyoheyetşí s. radioastronom. 
+radiyojurnal s. radiojurnal. 
+radiyokaberleşme s. radiocomunicaŃie. 
+radiyokabletúwğí s. radioreceptor. 
+radiyokomunikasiya s. radiocomunicaŃie. 
+radiyolaşkan adj. radioficat. 
+radiyolaşmak v.i. a se radiofica. 
+radiyolaştîrîlmak v.i. a se radiofica. 
+radiyolaştîrma s. radioficare. 
+radiyolaştîrmak v.t. a radiofica. 
+radiyolaştîruw s. radioficare. 
+radiyoloğik adj. radiologic. 
+radiyoloğiya I. adj. radiologic. II. s. radiologie. 
+radiyoloğiyağî s. radiolog. 
+radiyoloğiyalî adj. radiologic. 
+radiyolok s. radiolog. 
+radiyolokasiya s. (tel.) radiolocaŃie; radar. 
+radiyolokasiyağî s. (tel.) radiolocator; radar. 
+radiyonakliyeğí s. radioemiŃător. 
+radiyoneşretílgen adj. radiodifuzat. 
+radiyoneşretílmek v.i. a se radiodifuza. 
+radiyoneşretme s. radiodifuzare; radiodifuziune. 
+radiyoneşretmek v.t. a radiodifuza. 
+radiyoneşretúwğí s. radioemiŃător. 
+radiyoneşriyat s. radiotransmisie; emisiune radio. 
+radiyoneşriyatlî adj. radiodifuzat. 
+radiyosedalî adj. radiofonic. 
+radiyosedalîk s. radiofonie. 
+radiyosîzga s. radiografie. 
+radiyoskopiya s. radioscopie. 
+radiyotedaviy s. (med.) radioterapie. 
+radiyotelegrafiya s. radiotelegrafie. 
+radiyotelegrafiyağî s. radiotelegrafist. 
+radiyotelegrafî s. radiogramă. 
+radiyoterapiya s. (med.) radioterapie. 
+radiyouzaksîzga s. radiogramă. 
+radiyouzaksîzgağî s. radiotelegrafist. 
+radiyouzaksîzgağîlîk s. radiotelegrafie. 
+radiyum s. (chim.) radiu. 
+rafinat adj. rafinat. 
+rafineriya s. rafinărie. 
+raft s. raft. // ●raftka salmak — a lăsa pentru altă dată. 
+ragbet s. căutare; trecere; simpatie; succes. // ●ragbet kórmek — a avea căutare; a fi cerut. 
+ragbetlí adj. căutat; cerut; simpatizat; atractiv; îmbietor. 
+ragbetsíz adj. nesimpatizat; neatractiv. 
+ragîp adj. doritor; dornic. 
+Ragîp s. (antrop. m., arab.) "Cel dornic" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam). 
+ragmen postp. deşi; cu toate că; măcar că. 
+rağe s. rajah. 
+rahibe s. (relig.) călugăriŃă. 
+rahim I. adj. preamilostiv; milos; compătimitor. II. s. (anat.) uter; mitră. 
+Rahim s. (antrop. m., arab.) "Cel iertător" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam). 
+Rahim, El- s. (relig., arab.) "Cel iertător şi milostiv"; “Cel ce binecuvântează şi dăruieşte bunăstarea supuşilor Săi” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah). 
+Rahime s. (antrop. f., arab.) “Cea iertătoare”. 
+rahip s. (relig.) călugăr. 
+rahip-dudukuş s. (orn.) papagal-călugăr (lat., Myiopsitta monachus). 
+rahiplík s. (relig.) călugărie. 
+rahit s. (med.) rahitism. 
+rahitik adj. (med.) rahitic. 
+rahitism s. (med.) rahitism. 
+rahitlí adj. (med.) rahitic. 
+Rahman s. (topon.) Rahman (jud. Tulcea). 
+Rahman, El- s. (relig., arab.) "Cel priitor"; “Cel ce binecuvântează cu nepărtinire toate vietăŃile” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah). 
+rahmaniy adj. divin. 
+rahmet s. 1. (relig.) iertare divină. 2. (fig.) ploaie. // ●rahmet okîmak — (relig.) a cere iertare; a avea milă; a se îndura. // ●Alla(h) rahmet iylesín! — (relig.) Dumnezeu să-l ierte!; 
+rahmetlí s., adj. răposat; decedat. //●rahmetlí bolmak — a deceda. 
+rahne s. breşă; spărtură. 
+rahvan I. adv. în buiestru. II. s. (d. mersul calului) buiestru. 
+rakam I. adj. cantitativ. II. s. 1. cifră; număr. 2. cantitate; sumă. // ●rakamînda otîrmak — a se cifra la…; // ●bo sayî úş rakamdan múrekkep — acest număr este alcătuit din trei cifre. ●dayimiy rakam — (mat.) constantă. ●múrekkep rakam — (mat.) număr complex. ●roman rakamlarî — (mat.) cifre romane. 
+rakamîşeşílgen adj. descifrat. 
+rakamlama s. cifru; cod; codificare. 
+rakamlamak v.t. a cifra; a codifica. 
+rakamlangan adj. cifrat; codificat. 
+rakamlanmak v.i. a se cifra; a se codifica. 
+rakamlî adj. 1. cifrat; codificat. 2. cantitativ. 
+rakamşeşúwí s. descifrare. 
+raketa s. 1. rachetă; proiectil. 2. (sport) rachetă. // ●raketa şaptîrmak — a lansa o rachetă. //●raketa tayanagî — (mil.) bază de lansare a rachetelor. 
+raketagakarşî adj. (mil.) antirachetă. 
+rakik adj. 1. tandru; sensibil. 2. milos; compătimitor. 
+rakip s. concurent; rival; adversar. 
+Rakip, El- s. (relig., arab.) "Priveghetorul"; “Cel cu băgare de seamă la toate faptele vieŃuitoarelor” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah). 
+rakiplík s. concurenŃă; rivalitate. 
+rakit adj. (d. o apă) stătătoare. 
+rakî s. rachiu. 
+rakîs s. 1. dans. 2. (fiz.) pendulare; oscilare. // ●rakîs etmek — a pendula. 
+rakîslî adj. (fiz.) oscilant. 
+rakkas s. 1. dansator; balerin. 2. (fiz.) pendul. 
+rakkase s. dansatoare; balerină. 
+rakkaselík s. balet. 
+rakkaslîk s. balet. // ●rakkaslîk temaşasî — spectacol de balet. 
+rakle s. pupitru; bancă; despărŃitură. 
+rakort s. (tehn.) racord. 
+raksetmek v.i., v.t. a dansa; a face balet. 
+raksetúw s. balet. 
+raktam s. chei; debarcader. //●raktamga yaklaşamîz — (nav.) acostăm. 
+raktamlama s. debarcare. 
+raktamlamak v.t. a debarca. 
+raktamlangan adj. debarcat. 
+raktamlaw s. debarcare. 
+rakun s. (zool.) raton; ursuleŃ-spălător (lat., Procyon sp.). 
+ram adj. (d. oameni) supus; plecat. 
+Ramazan s. 1. (relig., la musulmani) post. 2. (cron.) a 9-a lună a anului conform calendarului musulman. //●Ramazan bayramî — (relig., la musulmani) sărbătoarea Ramazanului. ●Ramazan el-múbarek — (cron.) Ramazan cel sfânt. 
+Ramazan-Kóy s. (topon.) ("Satul Postului") Cişmeaua Nouă (jud. Tulcea). 
+rampa s. 1. rampă; platformă. 2. (nav.) ponton; punte. 3. (la drumuri) pantă. 
+randament s. randament. 
+randevuw s. întâlnire. 
+Raniye s. (antrop. f., arab.) “Privire’. 
+raport s. raport; notă; comunicare. //●raport bermek — a raporta. 
+raportşî s. (d. persoane) raportor. 
+rapsodiya s. (muz.) rapsodie. 
+raptiye s. clamă; agrafă. 
+raptiyelemek v.t. a prinde cu o clamă; a prinde cu o agrafă. 
+rasat s. (astr.) observare. 
+rasatkana s. (astr.) observator. 
+rasiyonal adj. raŃional. 
+rasiyonalğî s. raŃionalist. 
+rasiyonalism s. raŃionalism. 
+rasiyonalist s. raŃionalist. 
+rasiyonallîk s. raŃionalism. 
+raspa s. 1. răzuire. 2. răzătoare; răzuitoare. 3. (med.) raclaj; chiuretaj. 
+raspalama s. răzuire; raclare. 
+raspalamak v.t. a răzui; a racla. 
+raspalangan adj. răzuit; raclat. 
+raspalanmak v.i. a se răzui; a fi raclat. 
+raspalî adj. răzuit; raclat. 
+rastkele I. adj. întâmplător; aleator; incidental; accidental; sporadic. II. adv. la întâmplare; la nimereală; pe dibuite; alandala. 
+rastkelesewda s. aventură; legătură amoroasă întâmplătoare. 
+rastkelesewdağî s. aventurier; persoană care caută aventuri amoroase. 
+rastkelgen adj. nimerit. 
+rastkelme s. nimerire; nimereală. 
+rastkelmek v.i. 1. a întâlni; a găsi. 2. a nimeri; a atinge. 3. a se nimeri; a se pomeni; a se trezi; a pica întâmplător. 
+rastkelúw s. întâmplare; hazard. 
+rastlama s. nimerire; nimereală; coincidenŃă. 
+rastlamak v.t. a întâlni; a nimeri. 
+rastlanaturgan adj. întâlnit repetitiv; reîntâlnit. 
+rastlangan adj. nimerit. 
+rastlanmagan adj. nemaipomenit; nemaivăzut. 
+rastlanmak v.i. a se întâlni; a se găsi; a se nimeri; a coincide. 
+rastlanmaykalgan adj. nemaipomenit; nemaivăzut. 
+rastlaşkan adj. 1. nimerit; care coincide. 2. întâlnit; găsit. 
+rastlaşma s. coincidenŃă; întâlnire; joncŃiune. 
+rastlaşmak v.i. 1. a se întâlni; a se găsi. 2. a se nimeri; a coincide. //●kópten berítlí rastlaşmadîk — nu ne-am întâlnit de mult. 
+rastlayturma s. întâlnire repetitivă; reîntâlnire.
 `;
 export default rawDictionaryText;
