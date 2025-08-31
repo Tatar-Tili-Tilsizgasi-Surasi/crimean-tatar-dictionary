bc3fe39bdfd025652d09a816cdabe09f10c31536
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-08-31 11:37:46 +0200

    Update p.ts

--- data/dictionary/p.ts
+++ data/dictionary/p.ts
@@ -1,2 +1,39 @@
-const rawDictionaryText = ``;
+const rawDictionaryText = `
+Paçifik-altîn-ğawunkuşî s. (orn.) ploier de Pacific auriu (lat., Pluvialis fulva). 
+Paçifik-altîn-suwtorgayî s. (orn.) ploier de Pacific auriu (lat., Pluvialis fulva). 
+Paçifik-Dalayî/Paçifik-Deriyasî s. (hidron.) Oceanul Pacific. 
+Paçifik-ğadîragî s. (orn.) fâsă de Pacific (lat., Anthus rubescens). 
+padíşa s. padişah; sultan. 
+padíşalîk s. regalitate. 
+pafta s. 1. pafta. 2. bulină. 3. (la hărŃi) carou; plan topografic. 
+pagîr s. (bot.) leurdă (lat., Allium ursinum). 
+pagoda s. pagodă. 
+pagot-şegertkíğísí s. (orn.) graur de pagodă (lat., Sturnus pagodarum). 
+pagot-sîyîrşîgî s. (orn.) graur de pagodă (lat., Sturnus pagodarum). 
+pağay s. (bot.) meişor; mohor-roşu (lat., Panicum sanguinale). 
+paha s. cost; preŃ. 
+paj s. paj; kralga kîzmet yapkan bay soyîndan er bala. 
+pak adj. curat; pur. 
+paket s. pachet. 
+paketleme s. împachetare; ambalare. 
+paketlemek v.t. a împacheta; a ambala. 
+paketlengen adj. împachetat; ambalat. 
+paketlenmek v.i. a se împacheta; a se ambala. 
+pakize adj. f. curată; pură. 
+pakîl adv. intenŃionat; înadins; anume. 
+pakîlday adj. ostentativ. 
+pakîlğî s. cel ce face înadins. 
+Pakîstan s. (topon.) Pakistan. 
+pakîstanlî s., adj. pakistanez. 
+pakla s. 1. zală; inel. (bot.) fasole (lat., Phaseolus vulgaris). 2. (bot.) bob; măzăriche (lat., Vicia faba). // ●paklanî awuzundan şîgarmak — a scăpa porumbelul din gură. 
+paklağî adj. leguminos. 
+pakla-kazî s. (orn.) gâscă de semănătură (lat., Anser fabalis). 
+paklakîrî adj. castaniu deschis. 
+paklama s. curăŃare; purificare; mântuire. 
+paklamak v.t. a curăŃa; a purifica; a mântui. 
+paklangan adj. curăŃat; purificat; mântuit. 
+paklanmak v.i. a se curăŃa; a se purifica; a se mântui. 
+paklasî, meş- s. (bot.) soia (lat., Glycine hispida; Glycine maxima). 
+paklasî, soya- s. (bot.) soia (lat., Glycine hispida; Glycine maxima).
+`;
 export default rawDictionaryText;
